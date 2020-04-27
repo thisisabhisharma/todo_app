adb2c91ed118ea79bb0620eb965c8799a47d370a
thisisabhisharma <38296451+thisisabhisharma@users.noreply.github.com>
2020-04-27 23:45:03 +0530

    user profile icon

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="signin">
+  <div id="divid" class="signin">
     <h1>This is an sign-in page</h1>
+    <!-- {{ this.$store.state.test }} -->
+
     <facebook-login
       class="button"
       appId="529086384442607"
@@ -9,30 +11,83 @@
       @sdk-loaded="sdkLoaded"
     >
     </facebook-login>
+
+    <div id="userProfile">
+      <img
+        @click="imgClick()"
+        id="userPic"
+        src="https://platform-lookaside.fbsbx.com/platform/profilepic/?asid=2924548724330152&height=50&width=50&ext=1590324315&hash=AeRa8OHDFgnlWkJ7"
+        alt=""
+      />
+    </div>
+    <div id="userProfile2">
+      <p>{{ this.$store.state.profile.name }}</p>
+      <p>{{ this.$store.state.profile.email }}</p>
+    </div>
+    <!-- <p>url: {{ this.$store.state.imageURL.data.url }}</p> -->
+    <!-- <img v-bind:src="imgName()" /> -->
   </div>
-  
 </template>
 
 <script>
 import facebookLogin from "facebook-login-vuejs";
+// import Vue from 'vue'
+// import Vuex from 'vuex'
+
+// var URL = this.$store.state.imageURL.data.url
 
 export default {
   name: "SignIn",
   components: {
     facebookLogin,
   },
+
   methods: {
+    // imgName: function() {
+
+    //   console.log("pic link is", this.$store.state.imageURL.data.url)
+    //   return this.$store.state.imageURL;
+    // },
+
+    imgClick() {
+      console.log("logo onclick fired");
+      var x = document.getElementById("userProfile2");
+      console.log("this is x in img = " + x.style.display);
+      if (x.style.display == "none") {
+        console.log("img if condition fired ");
+        x.style.display = "block";
+      } else {
+        console.log("img else fired ");
+        console.log('');
+        x.style.display = "none";
+      }
+    },
+    loginClick() {
+      console.log(" login onclick fired");
+      var x = document.getElementById("userProfile");
+      console.log("this is x in login = " + x.style.display);
+      if (x.style.display == "none") {
+        console.log("login inside if ");
+
+        x.style.display = "block";
+      } else {
+        console.log("login inside else ");
+
+        x.style.display = "none";
+      }
+    },
     getUserData() {
       this.FB.api(
         "/me",
         "GET",
         { fields: "id,name,email,picture" },
         (userInformation) => {
-          console.warn("User profile data", userInformation);
-          this.personalID = userInformation.id;
-          this.email = userInformation.email;
-          this.name = userInformation.name;
-          this.photo = userInformation.picture;
+          console.log("User profile data", userInformation);
+          this.$store.state.profile.employee_id = userInformation.id;
+          this.$store.state.profile.email = userInformation.email;
+          this.$store.state.profile.name = userInformation.name;
+          this.picture = userInformation.picture;
+          this.$store.state.imageURL = userInformation.picture;
         }
       );
     },
@@ -45,6 +100,7 @@ export default {
       this.isConnected = true;
       this.getUserData();
       alert("You have successfully Logged In");
+      this.loginClick();
     },
     onLogout() {
       this.isConnected = false;
@@ -60,5 +116,21 @@ export default {
   margin: auto;
   width: 10%;
   margin-top: 9rem;
+}
+#userPic {
+  margin-top: -4rem;
+  margin-right: 1rem;
+  border: 3px solid #3b55a0;
+  border-radius: 30px;
+  float: right;
+}
+#userProfile {
+  display: none;
+}
+#userProfile2 {
+  display: none;
+  border: 3px solid #3b55a0;
+  float: right;
+  color: white;
 }
 </style>
